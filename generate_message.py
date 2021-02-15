463eaeb56effe6e1a055e89076296a5bdf891474
sumitalamkhan <31077557+sumitalamkhan@users.noreply.github.com>
2021-02-15 11:51:28 +0600

    Update generate_message.py

--- generate_message.py
+++ generate_message.py
@@ -35,20 +35,19 @@ def generate_message():
 def message():
     n=int(count())
     ptg=(abs(n-2)/2)*100
-    introduction="""My Qtpi,
+    introduction="""INSERT YOUR ADJECTIVE,
 Hope you are nice and cute by the grace of Almighty.\n"""
     cnt="\nYou have solved {} problem(s) today.".format(n)
     if(n<2):
         vrdc="That's {} % less than your daily goal.".format(ptg)
-        opn="Look queen, I know you have a kingdom to run. But can't you solve 2 problems in 24 hours? 2 P R O B L E M S -_- . I want no excuses. Hope you won't disappoint from the next day. Keep that in mind -_-."
+        opn="INSERT YOUR ANGRY TEXT HERE"
     elif(n==2):
         vrdc="All though you have completed your daily goal, my expectations from you, is much higher. You are on the edge. Hope you will solve more from the next day."
         opn=""
     else:
         vrdc="That's {} % more than your daily goal. ".format(ptg)
-        opn="""Queen , o my queen, have you not set codeforces on flames ! Sad for Mike Mirzayanov tho. 
-Take my heartiest of congratulations. I hope you will keep this up, always. """
-    conc="\nI am fine. How's your enchanted mirror doing? \n"
+        opn=""INSERT YOUR HAPPY TEXT HERE"""
+    conc="\INSERT YOUR CONCLUSIONARY TEXT HERE\n"
     a,b=get_sl()
     if(n>=2):
         gift=good(a)
@@ -60,6 +59,6 @@ Take my heartiest of congratulations. I hope you will keep this up, always. """
         updt_sl(0,a,b)
     ps2=generate_message()
     sign="""\nYour's always,
-    SAK"""
+    INSERT YOUR INITIALS HERE"""
     msg=introduction+cnt+vrdc+opn+"\n"+conc+ps1+ps2+sign
     return msg
